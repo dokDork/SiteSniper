33d87e018c82dde5607d4427222f5a240eeb0993
luigi.capuzzello <luigi.capuzzello@gmail.com>
2025-09-06 13:26:09 -0400

    added mimikatz.exe on windows stuff

--- siteSniper.sh
+++ siteSniper.sh
@@ -910,7 +910,6 @@ echo "https://github.com/jpillora/chisel/releases/download/v1.7.4/chisel_1.7.4_l
 echo "https://github.com/hugsy/gdb-static/raw/master/gdb-7.10.1-x32" | sudo tee -a download.txt
 echo "https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64" | sudo tee -a download.txt
 echo "https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32" | sudo tee -a download.txt
-echo "https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip" | sudo tee -a download.txt
 
 
 
@@ -970,6 +969,7 @@ https://github.com/andrew-d/static-binaries/raw/master/binaries/windows/x86/nmap
 https://github.com/andrew-d/static-binaries/raw/master/binaries/windows/x86/nping.exe
 https://github.com/andrew-d/static-binaries/raw/master/binaries/windows/x86/ncat.exe
 https://github.com/jpillora/chisel/releases/download/v1.7.4/chisel_1.7.4_windows_386.gz
+https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip
 EOF
 # Scarica i file utilizzando wget
 printf "\n===================================\n"
